77c6f2c42504f9aafe28d7ff6d73cae630b4285c
PolskiEMS <info@polskiems.pl>
2026-06-09 21:30:46 +0200

    Remove unnecessary closing div tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function LoginPage() {
                     </Link>
                   );
                 })}
-              </div>
+              
             </Card>
           </div>
 
